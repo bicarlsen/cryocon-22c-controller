a33788f877fd0ab9af798fc9f7f21c431b986864
Brian Carlsen <carlsen.bri@gmail.com>
2022-04-08 12:42:18 +0200

    Updated version. Added version file.

--- cryocon_22c_controller/__init__.py
+++ cryocon_22c_controller/__init__.py
@@ -1,1 +1,3 @@
+from ._version import __version__
+
 from cryocon_22c_controller.cryocon_controller import CryoconController
--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 import setuptools
 
+# get __version__
+exec( open( 'cryocon_22c_controller/_version.py' ).read() )
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+project_urls = {
+    'Source Code': 'https://github.com/bicarlsen/cryocon-22c-controller',
+    'Bug Tracker': 'https://github.com/bicarlsen/cryocon-22c-controller/issues'
+}
+
 setuptools.setup(
     name="cryocon-22c-controller",
-    version="0.0.3.post1",
+    version=__version__,
     author="Brian Carlsen",
     author_email="carlsen.bri@gmail.com",
     description="A package for controlling CryoCon 22C Temperature Controllers",
@@ -13,6 +21,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     keywords=['cryocon', 'cryocon 22c', 'temperature controller', 'cryostat', 'cryo'],
     url="https://github.com/bicarlsen/cryocon-22c-controller",
+    project_urls = project_urls,
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
